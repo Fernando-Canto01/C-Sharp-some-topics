8385b0f7c72da9c34f0c9f27147b71aa6e6a9382
Fernando José Canto Cetina <111129143+Fernando-Canto01@users.noreply.github.com>
2023-11-23 15:11:37 -0600

    Add files via upload

---
+++ Event_2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Event_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Publisher publisher = new Publisher();
+        Suscriber suscriber = new Suscriber();
+        publisher.Name = "Fernando José Canto Cetina";
+        publisher.NamePublication = "C#";
+        suscriber.Suscribe(publisher);
+        suscriber.Notify();
+    }
+}
+
+
+public class Publisher
+{
+    public string Name { get; set; }
+    public string NamePublication { get; set; }
+    public delegate void Publication();
+    public event Publication EventPublication;
+
+    public void InvokeEvent()
+    {
+        EventPublication?.Invoke();
+    }
+
+}
+
+
+public class Suscriber
+{
+    Publisher _Publisher;
+    public void Suscribe(Publisher publisher) => _Publisher = publisher;
+    public void AddHeaderFormat() => _Publisher.EventPublication += delegate ()
+    {
+        System.Console.WriteLine($"Name:{_Publisher.Name} / Title:{_Publisher.NamePublication}");
+    };
+
+    public void AddBodyFormat() => _Publisher.EventPublication += delegate ()
+    {
+        System.Console.WriteLine($"C# es un lenguaje de programación desarrollado por Microsoft, orientado a objetos");
+    };
+
+    public void AddDateFormat() => _Publisher.EventPublication += delegate ()
+    {
+        System.Console.WriteLine($"Date: {DateTime.Now}");
+    };
+
+    public void Notify()
+    {
+        AddHeaderFormat();
+        AddBodyFormat();
+        AddDateFormat();
+        _Publisher.InvokeEvent();
+    }
+
+
+}
